2069aa418d5d652e026f1327537e517ccabef0f8
unknown <patricl.gelera1@gmail.com>
2022-09-20 17:10:56 +0800

    jenkins

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 const Mysql         = require('mysql');
 const Constants     = require("./constants");
 
-var connection      = Mysql.createConnection(Constants.DATABASE);
+var connection = Mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "hh",
+	port: 3306,
+});
 
 connection.connect(function(err) {
     if (err) throw err;
